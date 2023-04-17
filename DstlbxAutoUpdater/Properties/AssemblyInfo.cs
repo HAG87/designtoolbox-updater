9193de2cac9ddea98728a3fd5930324896721d0b
bump-laptop <atelierbump@gmail.com>
2023-04-17 13:56:44 -0600

    d115572 - Version updated to 2.9.5.3

--- DstlbxAutoUpdater/Properties/AssemblyInfo.cs
+++ DstlbxAutoUpdater/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.9.5.2")]
-[assembly: AssemblyFileVersion("2.9.5.2")]
+[assembly: AssemblyVersion("2.9.5.3")]
+[assembly: AssemblyFileVersion("2.9.5.3")]
 [assembly: NeutralResourcesLanguage("en")]
